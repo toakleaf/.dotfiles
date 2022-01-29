a5b14556a2d3c070146881f287184b1e6e8d41ae
Tyler Oakleaf <tyleroakleaf@gmail.com>
2022-01-29 15:16:04 -0500

    tmux plugin install in init file

--- mac_init.sh
+++ mac_init.sh
@@ -22,7 +22,8 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 # see: https://gist.github.com/kevin-smets/8568070
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 # see: https://github.com/jeffreytse/zsh-vi-mode
-# git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH/custom/plugins/zsh-vi-mode
+git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH/custom/plugins/zsh-vi-mode
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
